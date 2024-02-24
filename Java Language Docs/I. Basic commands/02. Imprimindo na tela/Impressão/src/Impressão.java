01d2a38d5494739f9789b4592005034377146ad5
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2024-02-24 20:23:31 -0300

    Delete Java Language Docs/I. Basic commands directory

--- Java Language Docs/I. Basic commands/02. Imprimindo na tela/Impressão/src/Impressão.java
+++
@@ -1,7 +0,0 @@
-public class Impressão {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Esse é meu primeiro código em Java");
-        System.out.println ("Espero que goste");
-        System.out.println("Vamos codar");
-    }
-}
