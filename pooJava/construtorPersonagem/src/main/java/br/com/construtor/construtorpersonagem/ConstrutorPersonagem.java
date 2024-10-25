b63e6544696c9899266fe7b03d0c922e97098ac6
Gabriel S. Olavo <gabrielsantosolavo101@gmail.com>
2024-10-25 01:46:02 -0300

    complemento

--- pooJava/construtorPersonagem/src/main/java/br/com/construtor/construtorpersonagem/ConstrutorPersonagem.java
+++ pooJava/construtorPersonagem/src/main/java/br/com/construtor/construtorpersonagem/ConstrutorPersonagem.java
@@ -2,13 +2,20 @@
 package br.com.construtor.construtorpersonagem;
 
 import entidade.Personagem;
+import javax.swing.JOptionPane;
 
-public class ConstrutorPersonagem {
-
-    public static void main(String[] args) {
+public class ConstrutorPersonagem 
+{
+    public static void main(String[] args) 
+    {
         
         // Declaração de objetos, instânciamento e passagem de parâmetro do Construtor
-        Personagem personagem = new Personagem("Legolas", "Arqueiro");
+        // Usando janela gráfica do Java
+        
+        String nome = JOptionPane.showInputDialog(null, "Nome do personagem: ");
+        String classe = JOptionPane.showInputDialog(null, "Classe do Personagem: ");
+        
+        Personagem personagem = new Personagem(nome, classe);
         personagem.mostrarStatus();
     }
 }
--- pooJava/construtorPersonagem/src/main/java/entidade/Personagem.java
+++ pooJava/construtorPersonagem/src/main/java/entidade/Personagem.java
@@ -1,6 +1,9 @@
 package entidade;
 
-public class Personagem {
+import javax.swing.JOptionPane;
+
+public class Personagem 
+{
     // Declaração de Atributos
     
     private String nome;
@@ -13,64 +16,86 @@ public class Personagem {
     Serve para inicializar atributos, chamar métodos da Class,  pode ter ou não parâmetros  */
     // SINTAXE -> <moficador> <nomeDaClasse>
     
-    public Personagem (String nome, String classe) {
+    public Personagem (String nome, String classe) 
+    {
         this.nome = nome;
         this.classe = classe;
         nivel = 10;
         vida = nivel * 5000;
         
-        if (classe.equals("Arqueiro")) {
+        if (classe.equals("Arqueiro")) 
+        {
             forca = nivel * 3;
-        } else {
+        } 
+        else 
+        {
             forca = nivel;
         }
     }
     
     // Métodos Comuns
     
-    public void mostrarStatus() {
-        System.out.printf("Nome: %s\nNivel: %d\nVida: %d\nClasse: %s\nForça: %d", nome, nivel, vida, classe, forca);
+    public void mostrarStatus() 
+    {
+        String status = String.format("Nome: %s\nNivel: %d\nVida: %d\nClasse: %s\nForça: %d",
+            nome, 
+            nivel, 
+            vida, 
+            classe, 
+            forca);
+        
+        JOptionPane.showMessageDialog(null, status, "Status do Personagem", JOptionPane.INFORMATION_MESSAGE);
     }
     
     // Métodos Getters e Setters
     
-    public String getNome() {
+    public String getNome() 
+    {
         return nome;
     }
     
-    public void setNome(String nome) {
+    public void setNome(String nome) 
+    {
         this.nome = nome;
     }
     
-    public int getNivel() {
+    public int getNivel() 
+    {
         return nivel;
     }
     
-    public void setNivel(int nivel) {
+    public void setNivel(int nivel) 
+    {
         this.nivel = nivel;
     }
     
-    public int getForca() {
+    public int getForca() 
+    {
         return forca;
     }
     
-    public void setForca(int forca) {
+    public void setForca(int forca) 
+    {
         this.forca = forca;
     }
     
-    public int getVida() {
+    public int getVida() 
+    {
         return vida;
     }
     
-    public void setVida(int vida) {
+    public void setVida(int vida) 
+    {
         this.vida = vida;
     }
     
-    public String getClasse() {
+    public String getClasse() 
+    {
         return classe;
     }
     
-    public void setClasse(String classe) {
+    public void setClasse(String classe) 
+    {
         this.classe = classe;
     }
 }
